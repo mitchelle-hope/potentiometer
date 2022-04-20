216d4c3b95dfc07428cbb59e8e90b24f07ddc8c7
Mitchelle-hope <99388852+mitchelle-hope@users.noreply.github.com>
2022-04-20 12:15:28 +0300

    Add files via upload

---
+++ potentipometer1.ino
@@ -0,0 +1,23 @@
+
+#include <Servo.h>
+
+Servo myservo;
+
+int potpin = 0 ;
+int val;
+
+
+void setup(){
+  myservo.attach(9);
+
+}
+  
+ 
+
+void loop()
+{
+  val = analogRead(0);
+  val = map(val,0,1023,0,180);
+  myservo.write(val);
+  delay(15); 
+}
